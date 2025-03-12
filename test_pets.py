ed249c42506ca68fc9e9de8b286d698e3ae4872e
Santiago <santiago.lopez1@primestone.com>
2025-03-12 11:53:44 -0500

    PUT and DELETE test methods implemented

--- test_pets.py
+++ test_pets.py
@@ -35,5 +35,33 @@ class TestPets:
         assert response_data["name"] == self.pet_data["name"]
         assert response_data["status"] == self.pet_data["status"]
 
+    def test_update_pet(self):
+        #New data modified pet
+        pet_new_data = {
+        "id": 122,
+        "name": "zeus",
+        "photoUrls": [],
+        "tags": [],
+        "status": "sold"
+        }
+        #Send PUT Request
+        response = requests.put(f"{self.base_url}/pet",json=pet_new_data)
+
+        #ASSERTIONS 
+        assert response.status_code == 200
+        response_data = response.json()
+        assert response_data['id'] == pet_new_data['id']
+        assert response_data["name"] == pet_new_data["name"]
+        assert response_data["status"] == pet_new_data["status"]
+
+    def test_delete_pet(self):
+        #Send Delete request
+        response = requests.delete(f"{self.base_url}/pet/{self.pet_data['id']}")
+
+        #ASSERTIONS
+        assert response.status_code == 200
+
+        
+
 
 
